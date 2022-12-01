5f8a3dc4f1bdca5d1f3e3b6965c4f83297bb6719
Don Jose <donnjondanmakal@gmail.com>
2022-12-01 16:43:51 +0530

    Final Code

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const connectbtn = document.getElementById("connectButton");
 const fundbtn = document.getElementById("fundButton");
+const balanceButton = document.getElementById("balanceButton");
+const withDraw = document.getElementById("withdrawButton");
 import { ethers } from "./ethers.js";
 import { abi, contractAddress } from "./constant.js";
 connectbtn.addEventListener("click", () => {
@@ -8,6 +10,13 @@ connectbtn.addEventListener("click", () => {
 fundbtn.addEventListener("click", () => {
   fund();
 });
+balanceButton.addEventListener("click", () => {
+  getBalance();
+});
+withDraw.addEventListener("click", () => {
+  withDra();
+});
+
 async function connect() {
   if (typeof window.ethereum != "undefined") {
     await window.ethereum.request({
@@ -19,7 +28,8 @@ async function connect() {
       "Please install MetaMask";
   }
 }
-async function fund(eth_amount) {
+async function fund() {
+  const eth_amount = document.getElementById("ethAmount").value;
   console.log(eth_amount);
   if (typeof window.ethereum != "undefined") {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -28,12 +38,50 @@ async function fund(eth_amount) {
 
     try {
       const transactionResponse = await contract.fund({
-        value: ethers.utils.parseEther(".1"),
+        value: ethers.utils.parseEther(eth_amount),
       });
+      await listenForTransactionMine(transactionResponse, provider);
     } catch (e) {
       console.log(e);
     }
   }
 }
 
-function listenForTransactionMine(transactionResponse, provider) {}
+function listenForTransactionMine(transactionResponse, provider) {
+  //13.23
+  console.log("Mining " + transactionResponse.hash);
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionRecipt) => {
+      console.log(
+        "Recieved with ",
+        transactionRecipt.confirmations + "Confirmations"
+      );
+      resolve();
+    });
+  });
+}
+
+async function getBalance() {
+  console.log("sj");
+  if (typeof window.ethereum != "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const balance = await provider.getBalance(contractAddress);
+    console.log(ethers.utils.formatEther(balance));
+  }
+}
+
+async function withDra() {
+  if (typeof window.ethereum != "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.withdraw();
+      await listenForTransactionMine(transactionResponse, provider);
+      // await transactionResponse.wait(1)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
